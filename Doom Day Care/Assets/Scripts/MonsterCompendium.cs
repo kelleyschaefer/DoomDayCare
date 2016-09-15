4dc4a9c6ea0ff0e91dce34165a96964ae3d268bf
kelleyschaefer <kelleyschaefer@Kelleys-MBP.domain>
2016-09-14 20:32:40 -0600

    Recipe generation began
    Began framework of how recipe generation will look. Not sure what all
    of these extra meta files Unity produced are, but they should likely be
    ignored by the .gitignore

---
+++ Doom Day Care/Assets/Scripts/MonsterCompendium.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "MonsterCompendium", menuName = "Monsters", order =1)]
+public class MonsterCompendium: ScriptableObject {
+	public Monster[] Monsters;
+
+	public string[] sideIngredients = {"Bones","Poop","Stone","Dirt","Mystery Meat", "Newt's Eye",
+		"Whisper Root", "Snickers Bar"};
+
+	public void InstantiateCompendium(){
+		
+		Monsters = new Monster[3];
+		Monster Chimera = new Monster ("Chimera", "Lizard Tail", "Lion Claw", sideIngredients[Random.Range(0,8)], sideIngredients[Random.Range(0,8)]);
+		Monster Phoenix = new Monster ("Phoenix", "Feather", "Flint and Steel", sideIngredients[Random.Range(0,8)], sideIngredients[Random.Range(0,8)]);
+		Monster Beholder = new Monster("Beholder", "Eyeball", "Tentacle", sideIngredients[Random.Range(0,8)], sideIngredients[Random.Range(0,8)]);
+
+		Monsters [0] = Chimera;
+		Monsters [1] = Phoenix;
+		Monsters [2] = Beholder;
+	}
+
+	public string fetchMonster(string[] Recipe){
+		foreach (Monster m in Monsters) {
+			string[] temp = m.getRecipe ();
+			foreach (string s in temp) {
+				if (s == Recipe [0]) {
+					return m.Name;
+				} 
+				else
+					break;
+			}
+		}
+		return "None";
+	}
+}
+
+/*
+ * Contains array that is the Recipe for the monster and the monster's name.
+ * In actual implementation this will hold all [base] information about a Monster in addition to Name and Recipe. 
+ * (Ie. Potential colors, personality, needs, price, whatever)
+ * */
+public class Monster{
+	
+	public string Name;
+	private string[] Recipe;
+			
+	public Monster(string name, string ing1, string ing2, string ing3, string ing4){
+		Recipe = new string[4];
+		Name = name;
+		Recipe [0] = ing1;
+		Recipe [1] = ing2;
+		Recipe [2] = ing3;
+		Recipe [3] = ing4;
+	}		
+					
+	public string[] getRecipe(){
+		return Recipe;
+	}
+
+	public void setRecipe(string[] newRecipe){
+		Recipe = newRecipe;
+	}
+
+}
---
+++ Doom Day Care/Assets/Scripts/RecipeGeneration.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+/*
+ * For the time being, we can stick this script onto some object that starts the prototype.
+ * When it begins, it'll instantiate the MonsterCompendium ScriptableObject
+ * This object will exist in memory and regardless of scene (Meaning it won't be necessary to delete/load it between scenes)
+ * 
+ * Currently everything is just strings and hard-coded. In the future, Ingredients will be objects themselves
+ * and Recipes will store Ingredient IDs
+ * */
+
+public class RecipeGeneration : MonoBehaviour {
+
+	//Scriptable object holding Monster object type (Not GameObject type) and collection of them for identification
+	//Monster object type holds the recipe necessary for creating the monster
+	public MonsterCompendium MC;
+
+	// Use this for initialization
+	//TODO: Put this into Awake()
+	void Start () {
+		
+		MC = (MonsterCompendium)ScriptableObject.CreateInstance (typeof(MonsterCompendium));
+		MC.InstantiateCompendium ();
+
+		foreach (Monster m in MC.Monsters){
+			Debug.Log (m.Name);
+		}
+
+		foreach (Monster m in MC.Monsters) {
+			string monster = "";
+			monster += m.Name + ", Recipe: ";
+			string[] temp = m.getRecipe ();
+			monster+= temp [0] + ", ";
+			monster+= temp [1] + ", ";
+			monster+= temp [2] + ", ";
+			monster+= temp [3];
+			Debug.Log (monster);
+		}
+
+		string[] rep = { "Eyeball", "Tentacle", "Something", "Something" };
+		Debug.Log(MC.fetchMonster (rep));
+
+	}
+
+	/*
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	*/
+}
