c39432c51ab151c88279bdfcdc0304744b09b492
Matt Lemon <u0575787@utah.edu>
2016-09-28 17:08:31 -0600

    monster world gen

--- DoomsDayCare/DoomsDayCare/Assets/Scripts/Cauldron.cs
+++ DoomsDayCare/DoomsDayCare/Assets/Scripts/Cauldron.cs
@@ -6,10 +6,13 @@ public class Cauldron : MonoBehaviour
 
     public GameObject player;
     public GameObject MonsterList;
+    public GameObject Monster_World_Prefab;
+    public GameObject monster_dialog_box;
+
 
 	// Use this for initialization
 	void Start () {
-	
+        monster_dialog_box.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -17,5 +20,14 @@ public class Cauldron : MonoBehaviour
 	
 	}
 
-
+    private void Add_Monster_To_World(Monster m)
+    {
+        GameObject pf = Monster_World_Prefab;
+        pf.GetComponent<Animator>().runtimeAnimatorController = m.controller;
+        pf.GetComponent<MonsterInteraction>().sayings = m.sayings;
+        pf.GetComponent<MonsterInteraction>().drop = m.drop;
+        pf.GetComponent<MonsterInteraction>().dialog_box = monster_dialog_box;
+        Vector3 location = new Vector3(Random.Range(-14, -6), Random.Range(-7, 3), 0);
+        GameObject go = (GameObject)Instantiate(pf, location, Quaternion.identity);
+    }
 }
--- DoomsDayCare/DoomsDayCare/Assets/Scripts/Monster.cs
+++ DoomsDayCare/DoomsDayCare/Assets/Scripts/Monster.cs
@@ -7,6 +7,7 @@ public class Monster
     public string name;
     public Ingredient[] recipe;
     public Sprite static_image;
-    public GameObject visual_prefab;
     public Ingredient drop;
+    public string[] sayings;
+    public UnityEngine.RuntimeAnimatorController controller;
 }
